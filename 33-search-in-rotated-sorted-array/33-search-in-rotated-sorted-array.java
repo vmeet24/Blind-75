d131709f078be6d6087b47201ae4cf5681ef9468
vmeet24 <47553376+vmeet24@users.noreply.github.com>
2022-05-22 00:44:33 -0400

    Time: 2 ms (6.50%), Space: 43 MB (19.33%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,24 +1,54 @@
 class Solution {
-     public int search(int[] nums, int target) {
-        int idx = -1;
+
+    //nlogn
+    //      public int search(int[] nums, int target) {
+    //         int idx = -1;
+    //         int low = 0;
+    //         int high = nums.length - 1;
+    //         idx = find(low, high, nums, target);
+    //         return idx;
+    //     }
+
+    //     private int find(int low, int high, int[] nums, int target) {
+    //         int mid = (int) Math.floor((high + low) / 2);
+    //         if (nums[mid] == target) {
+    //             return mid;
+    //         }
+    //         if (low < high) {
+    //             int left = find(low, mid - 1, nums, target);
+    //             int right = find(mid + 1, high, nums, target);
+    //             if (left != -1) {
+    //                 return left;
+    //             } else {
+    //                 return right;
+    //             }
+    //         }
+    //         return -1;
+    //     }
+
+    public int search(int[] nums, int target) {
         int low = 0;
         int high = nums.length - 1;
-        idx = find(low, high, nums, target);
-        return idx;
-    }
+        while (low <= high) {
+            int mid = (int) (Math.floor(high + low) / 2);
+            if (nums[mid] == target) {
+                return mid;
+            }
 
-    private int find(int low, int high, int[] nums, int target) {
-        int mid = (int) Math.floor((high + low) / 2);
-        if (nums[mid] == target) {
-            return mid;
-        }
-        if (low < high) {
-            int left = find(low, mid - 1, nums, target);
-            int right = find(mid + 1, high, nums, target);
-            if (left != -1) {
-                return left;
-            } else {
-                return right;
+            if (nums[low] <= nums[mid]) {
+                if (target < nums[mid] && target >= nums[low]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+
+            if (nums[mid] <= nums[high]) {
+                if (target > nums[mid] && target<= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
             }
         }
         return -1;
