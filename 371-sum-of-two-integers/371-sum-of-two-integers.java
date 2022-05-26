0f3c41c2e4726c95060cc05ad37a0cdb25c350db
vmeet24 <47553376+vmeet24@users.noreply.github.com>
2022-05-26 01:18:03 -0400

    Time: 0 ms (100.00%), Space: 40.4 MB (68.96%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.java
+++ 371-sum-of-two-integers/371-sum-of-two-integers.java
@@ -2,6 +2,7 @@ class Solution {
 
     public int getSum(int a, int b) {
         while (b != 0) {
+            // Storing this value in temp variable so that b doesnt get the updated value of a while doing XOR operation.
             int tmp = (a & b) << 1;
             a = a ^ b;
             b = tmp;
