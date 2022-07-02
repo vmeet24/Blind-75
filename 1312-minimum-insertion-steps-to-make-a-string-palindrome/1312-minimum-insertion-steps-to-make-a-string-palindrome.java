5495ae9545cefb216429c474e2af2397c9ac47e1
vmeet24 <47553376+vmeet24@users.noreply.github.com>
2022-07-02 02:32:32 -0400

    Time: 34 ms (78.95%), Space: 55.3 MB (6.75%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minInsertions(String s) {
+        char[] text1C = s.toCharArray();
+        char[] text2C = new StringBuilder(s).reverse().toString().toCharArray();
+        int[][] arr = new int[text1C.length + 1][text2C.length + 1];
+        for (int i = 0; i < text1C.length; i++) {
+            arr[i][0] = 0;
+        }
+        for (int i = 0; i < text2C.length; i++) {
+            arr[0][i] = 0;
+        }
+
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 1; j < arr[0].length; j++) {
+                if (text1C[i - 1] == text2C[j - 1]) {
+                    arr[i][j] = 1 + arr[i - 1][j - 1];
+                } else {
+                    arr[i][j] = Math.max(arr[i - 1][j], arr[i][j - 1]);
+                }
+            }
+        }
+        return text1C.length - arr[text1C.length][text2C.length];
+    }
+}
