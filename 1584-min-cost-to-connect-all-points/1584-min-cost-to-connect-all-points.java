25338489ba8b2e0ba0613414881387cc8321dd57
vmeet24 <47553376+vmeet24@users.noreply.github.com>
2022-07-05 20:49:21 -0400

    Time: 767 ms (32.57%), Space: 118.2 MB (44.23%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -40,38 +40,40 @@ class UnionFind {
 
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        int n = points.length;
-        ArrayList<int[]> allEdges = new ArrayList<>();
+        ArrayList<int[]> lst = new ArrayList<>();
 
-        // Storing all edges of our complete graph.
-        for (int currNext = 0; currNext < n; ++currNext) {
-            for (int nextNext = currNext + 1; nextNext < n; ++nextNext) {
-                int weight = Math.abs(points[currNext][0] - points[nextNext][0]) +
-                        Math.abs(points[currNext][1] - points[nextNext][1]);
-
-                int[] currEdge = { weight, currNext, nextNext };
-                allEdges.add(currEdge);
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                int weight = Math.abs(points[i][0] - points[j][0]) +
+                        Math.abs(points[i][1] - points[j][1]);
+                int[] node = { weight, i, j };
+                lst.add(node);
             }
         }
 
-        // Sort all edges in increasing order.
-        Collections.sort(allEdges, (a, b) -> Integer.compare(a[0], b[0]));
+        Collections.sort(lst, (x, y) -> {
+            return Integer.compare(x[0], y[0]);
+        });
 
-        UnionFind uf = new UnionFind(n);
-        int mstCost = 0;
-        int edgesUsed = 0;
+        // for (int[] is : lst) {
+        // System.out.println(Arrays.toString(is));
+        // }
 
-        for (int i = 0; i < allEdges.size() && edgesUsed < n - 1; ++i) {
-            int node1 = allEdges.get(i)[1];
-            int node2 = allEdges.get(i)[2];
-            int weight = allEdges.get(i)[0];
-
-            if (uf.union(node1, node2)) {
-                mstCost += weight;
-                edgesUsed++;
+        int res = 0;
+        int edgeCounted = 0;
+        UnionFind uc = new UnionFind(points.length);
+        for (int i = 0; i < lst.size() && edgeCounted < points.length - 1; i++) {
+            int x = lst.get(i)[1];
+            int y = lst.get(i)[2];
+            int weight = lst.get(i)[0];
+            if (uc.union(x, y) && edgeCounted < points.length - 1) {
+                res += weight;
+                edgeCounted++;
             }
+            // if (edgeCounted == points.length - 1) {
+            // break;
+            // }
         }
-
-        return mstCost;
+        return res;
     }
 }
