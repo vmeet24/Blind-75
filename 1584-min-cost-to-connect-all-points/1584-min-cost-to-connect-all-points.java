624a892834b2f85fecc5ff7e76d373f7780c1f07
vmeet24 <47553376+vmeet24@users.noreply.github.com>
2022-07-05 20:49:59 -0400

    Time: 829 ms (28.81%), Space: 122.9 MB (34.78%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,78 +1,75 @@
 class UnionFind {
-        int edgeCounted = 0;
-        int[] arr;
+    int edgeCounted = 0;
+    int[] arr;
 
-        public UnionFind(int length) {
-            arr = new int[length];
-            Arrays.fill(arr, -1);
-        }
-
-        private int find(int x) {
-            int parent = x;
-            int value = arr[x];
-            while (value >= 0) {
-                parent = value;
-                value = arr[parent];
-            }
-            return parent;
-        }
-
-        public boolean union(int x, int y) {
-            int parent1 = find(x);
-            int parent2 = find(y);
-
-            if (parent1 == parent2) {
-                return false;
-            }
-
-            if (arr[parent1] <= arr[parent2]) {
-                arr[parent1] = arr[parent1] + arr[parent2];
-                arr[parent2] = parent1;
-                arr[y] = parent1;
-            } else if (arr[parent1] > arr[parent2]) {
-                arr[parent2] = arr[parent1] + arr[parent2];
-                arr[parent1] = parent2;
-                arr[x] = parent2;
-            }
-            return true;
-        }
+    public UnionFind(int length) {
+        arr = new int[length];
+        Arrays.fill(arr, -1);
     }
 
+    private int find(int x) {
+        int parent = x;
+        int value = arr[x];
+        while (value >= 0) {
+            parent = value;
+            value = arr[parent];
+        }
+        return parent;
+    }
+
+    public boolean union(int x, int y) {
+        int parent1 = find(x);
+        int parent2 = find(y);
+
+        if (parent1 == parent2) {
+            return false;
+        }
+
+        if (arr[parent1] <= arr[parent2]) {
+            arr[parent1] = arr[parent1] + arr[parent2];
+            arr[parent2] = parent1;
+            arr[y] = parent1;
+        } else if (arr[parent1] > arr[parent2]) {
+            arr[parent2] = arr[parent1] + arr[parent2];
+            arr[parent1] = parent2;
+            arr[x] = parent2;
+        }
+        return true;
+    }
+}
+
 class Solution {
+
     public int minCostConnectPoints(int[][] points) {
+        int arrSize = points.length;
         ArrayList<int[]> lst = new ArrayList<>();
 
-        for (int i = 0; i < points.length; i++) {
-            for (int j = i + 1; j < points.length; j++) {
-                int weight = Math.abs(points[i][0] - points[j][0]) +
-                        Math.abs(points[i][1] - points[j][1]);
+        for (int i = 0; i < arrSize; i++) {
+            for (int j = i + 1; j < arrSize; j++) {
+                int weight = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
                 int[] node = { weight, i, j };
                 lst.add(node);
             }
         }
 
-        Collections.sort(lst, (x, y) -> {
-            return Integer.compare(x[0], y[0]);
-        });
-
-        // for (int[] is : lst) {
-        // System.out.println(Arrays.toString(is));
-        // }
+        Collections.sort(
+            lst,
+            (x, y) -> {
+                return Integer.compare(x[0], y[0]);
+            }
+        );
 
         int res = 0;
         int edgeCounted = 0;
-        UnionFind uc = new UnionFind(points.length);
-        for (int i = 0; i < lst.size() && edgeCounted < points.length - 1; i++) {
+        UnionFind uc = new UnionFind(arrSize);
+        for (int i = 0; i < lst.size() && edgeCounted < arrSize - 1; i++) {
             int x = lst.get(i)[1];
             int y = lst.get(i)[2];
             int weight = lst.get(i)[0];
-            if (uc.union(x, y) && edgeCounted < points.length - 1) {
+            if (uc.union(x, y)) {
                 res += weight;
                 edgeCounted++;
             }
-            // if (edgeCounted == points.length - 1) {
-            // break;
-            // }
         }
         return res;
     }
