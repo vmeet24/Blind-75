a9cc0d4d9a5313814a4c1d1929ac5d2668a4a4e6
vmeet24 <47553376+vmeet24@users.noreply.github.com>
2022-07-12 19:45:45 -0400

    Time: 10 ms (97.78%), Space: 46.8 MB (16.62%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -0,0 +1,48 @@
+class Solution {
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        // int[][] t = new int[text1.length()+1][text2.length()+1];
+        // for (int[] is : t) {
+        //     Arrays.fill(is, 0);
+        // }
+        // return longestCommonSubsequenceRecurrsion(text1, text2, text1.length(), text2.length(), t);
+
+        char[] text1C = text1.toCharArray();
+        char[] text2C = text2.toCharArray();
+        int[][] arr = new int[text1C.length + 1][text2C.length + 1];
+        for (int i = 0; i < text1C.length; i++) {
+            arr[i][0] = 0;
+        }
+        for (int i = 0; i < text2C.length; i++) {
+            arr[0][i] = 0;
+        }
+
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 1; j < arr[0].length; j++) {
+                if (text1C[i - 1] == text2C[j - 1]) {
+                    arr[i][j] = 1 + arr[i - 1][j - 1];
+                } else {
+                    arr[i][j] = Math.max(arr[i - 1][j], arr[i][j - 1]);
+                }
+            }
+        }
+        return arr[text1C.length][text2C.length];
+    }
+}
+//     private int longestCommonSubsequenceRecurrsion(String text1, String text2, int length1, int length2, int[][] t) {
+//         if (length1 == 0 || length2 == 0) {
+//             return 0;
+//         }
+//         if (t[length1][length2] != 0) {
+//             return t[length1][length2];
+//         } else {
+//             if (text1.toCharArray()[length1 - 1] == text2.toCharArray()[length2 - 1]) {
+//                 t[length1][length2] = 1 + longestCommonSubsequenceRecurrsion(text1, text2, length1 - 1, length2 - 1, t);
+//                 return t[length1][length2];
+//             } else {
+//                 t[length1][length2] = Math.max(longestCommonSubsequenceRecurrsion(text1, text2, length1 - 1, length2, t), longestCommonSubsequenceRecurrsion(text1, text2, length1, length2 - 1, t));
+//                 return t[length1][length2];
+//             }
+//         }
+//     }
+// }
