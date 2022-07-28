3392f94d979c8531e3c33c01a2eb4ae7af5e7198
Meet Vora <47553376+vmeet24@users.noreply.github.com>
2022-07-28 17:06:36 -0400

    Time: 1 ms (89.19%), Space: 41 MB (56.27%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
@@ -1,7 +1,7 @@
 class Solution {
 
     public int kthFactor(int n, int k) {
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n/2; i++) {
             if (n % i == 0) {
                 k--;
             }
@@ -9,6 +9,6 @@ class Solution {
                 return i;
             }
         }
-        return -1;
+        return k==1? n:-1;
     }
 }
