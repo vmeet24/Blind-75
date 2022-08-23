d8f57fe209ebcd783ea42090d01d9da2f63ba5f4
Meet Vora <47553376+vmeet24@users.noreply.github.com>
2022-08-23 17:34:51 -0400

    Time: 10 ms (62.52%), Space: 44.7 MB (80.80%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.java
@@ -1,37 +1,55 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        Map<Integer, Integer> start = new HashMap<>();
-        Map<Integer, Integer> end = new HashMap<>();
-        int maxTime = 0;
-        for (int i = 0; i < intervals.length; i++) {
-            if (!start.containsKey(intervals[i][0])) {
-                start.put(intervals[i][0], 1);
-            } else {
-                start.put(intervals[i][0], start.get(intervals[i][0]) + 1);
-            }
-            if (!end.containsKey(intervals[i][1])) {
-                maxTime = Math.max(intervals[i][1], maxTime);
-                end.put(intervals[i][1], 1);
-            } else {
-                end.put(intervals[i][1], end.get(intervals[i][1]) + 1);
-            }
-        }
+//         Map<Integer, Integer> start = new HashMap<>();
+//         Map<Integer, Integer> end = new HashMap<>();
+//         int maxTime = 0;
+//         for (int i = 0; i < intervals.length; i++) {
+//             if (!start.containsKey(intervals[i][0])) {
+//                 start.put(intervals[i][0], 1);
+//             } else {
+//                 start.put(intervals[i][0], start.get(intervals[i][0]) + 1);
+//             }
+//             if (!end.containsKey(intervals[i][1])) {
+//                 maxTime = Math.max(intervals[i][1], maxTime);
+//                 end.put(intervals[i][1], 1);
+//             } else {
+//                 end.put(intervals[i][1], end.get(intervals[i][1]) + 1);
+//             }
+//         }
 
-        int currInUse = 0;
-        int totalConference = 0;
-        for (int i = 0; i <= maxTime; i++) {
-            if (end.containsKey(i)) {
-                currInUse -= end.get(i);
+//         int currInUse = 0;
+//         int totalConference = 0;
+//         for (int i = 0; i <= maxTime; i++) {
+//             if (end.containsKey(i)) {
+//                 currInUse -= end.get(i);
+//             }
+//             if (start.containsKey(i)) {
+//                 if (totalConference >= currInUse + start.get(i)) {
+//                     currInUse += start.get(i);
+//                 } else {
+//                     totalConference = currInUse + start.get(i);
+//                     currInUse += start.get(i);
+//                 }
+//             }
+//         }
+//         return totalConference;
+        Arrays.sort(intervals, new Comparator<int[]>() {
+
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
             }
-            if (start.containsKey(i)) {
-                if (totalConference >= currInUse + start.get(i)) {
-                    currInUse += start.get(i);
-                } else {
-                    totalConference = currInUse + start.get(i);
-                    currInUse += start.get(i);
-                }
+        });
+
+        Queue<Integer> pq = new PriorityQueue<>();
+        pq.add(intervals[0][1]);
+        for (int i = 1; i < intervals.length; i++) {
+            if (!pq.isEmpty() && intervals[i][0] >= pq.peek()) {
+                pq.poll();
             }
+            pq.add(intervals[i][1]);
         }
-        return totalConference;
+        System.out.println(pq.size());
+        return pq.size();
     }
 }
