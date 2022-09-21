a9efb453394c215c85fb3c19ccad0a946c49ca02
Meet Vora <47553376+vmeet24@users.noreply.github.com>
2022-09-21 01:15:02 -0400

    Time: 4 ms (86.41%), Space: 44.5 MB (84.04%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -2,28 +2,22 @@ class Solution {
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
-        combinationSumHelper(candidates, target, res, new String(), 0, candidates.length - 1);
+        List<Integer> curr = new ArrayList<>();
+        combinationSumHelper(candidates, target, res, curr, 0, candidates.length - 1);
         return res;
     }
 
-    void combinationSumHelper(int[] candidates, int target, List<List<Integer>> res, String currList, int currSum, int n) {
+    void combinationSumHelper(int[] candidates, int target, List<List<Integer>> res, List<Integer> currList, int currSum, int n) {
         if (n == -1 || currSum > target) {
             return;
         }
 
         if (currSum == target) {
-            String[] r = currList.split(" ");
-            List<Integer> it = new ArrayList<>();
-            for (int i = 1; i < r.length; i++) {
-                try {
-                    it.add(Integer.parseInt(r[i]));
-                } catch (Exception e) {
-                    continue;
-                }
-            }
-            res.add(it);
+            res.add(new ArrayList<>(currList));
         } else if (currSum < target) {
-            combinationSumHelper(candidates, target, res, currList + " " + candidates[n], currSum + candidates[n], n);
+            currList.add(candidates[n]);
+            combinationSumHelper(candidates, target, res, currList, currSum + candidates[n], n);
+            currList.remove(currList.size() - 1);
             combinationSumHelper(candidates, target, res, currList, currSum, n - 1);
         }
     }
