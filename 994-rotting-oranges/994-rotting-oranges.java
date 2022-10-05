5c608bd406ee8479484173c89118c4e721f8bd93
Meet Vora <47553376+vmeet24@users.noreply.github.com>
2022-10-04 22:51:11 -0400

    Time: 5 ms (27.47%), Space: 42.5 MB (72.86%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -14,15 +14,12 @@ class Solution {
 
     public int orangesRotting(int[][] grid) {
         Queue<Pair> queue = new ArrayDeque<>();
-        List<Pair> rottenPairs = getRottenOranges(grid);
+        int freshOranges = getFreshOrangesAndAddRottenOranges(grid, queue);
         int n = grid.length;
         int m = grid[0].length;
         boolean[][] visited = new boolean[n][m];
         int[] delRow = { 0, -1, 0, 1 };
         int[] delCol = { -1, 0, 1, 0 };
-        for (Pair pair : rottenPairs) {
-            queue.add(new Pair(pair.row, pair.col, 0));
-        }
         int maxTime = 0;
         while (!queue.isEmpty()) {
             Pair rottenOrange = queue.poll();
@@ -34,36 +31,27 @@ class Solution {
                     int row = rottenOrange.row + delRow[i];
                     int col = rottenOrange.col + delCol[i];
                     if (row >= 0 && row < n && col >= 0 && col < m && grid[row][col] == 1 && !visited[row][col]) {
+                        freshOranges--;
                         grid[row][col] = 2;
                         queue.add(new Pair(row, col, rottenOrange.time + 1));
                     }
                 }
             }
         }
-        return checkRotten(grid) ? maxTime : -1;
+        return freshOranges == 0 ? maxTime : -1;
     }
 
-    List<Pair> getRottenOranges(int[][] grid) {
-        List<Pair> pair = new ArrayList<>();
-
+    int getFreshOrangesAndAddRottenOranges(int[][] grid, Queue<Pair> queue) {
+        int freshOranges = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 2) {
-                    pair.add(new Pair(i, j, 0));
+                    queue.add(new Pair(i, j, 0));
+                } else if (grid[i][j] == 1) {
+                    freshOranges++;
                 }
             }
         }
-        return pair;
-    }
-
-    boolean checkRotten(int[][] grid) {
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1) {
-                    return false;
-                }
-            }
-        }
-        return true;
+        return freshOranges;
     }
 }
