e07f27314099b14543d654c13b2550d520914f49
Meet Vora <47553376+vmeet24@users.noreply.github.com>
2022-10-11 13:52:19 -0400

    Time: 15 ms (48.55%), Space: 76.8 MB (15.54%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -0,0 +1,51 @@
+class Solution {
+
+    public int numEnclaves(int[][] grid) {
+        int[][] board = grid;
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        int[] deltaRow = { 0, -1, 0, +1 };
+        int[] deltaCol = { -1, 0, +1, 0 };
+
+        // first col and last col
+        for (int i = 0; i < visited.length; i++) {
+            if (board[i][0] == 1 && !visited[i][0]) {
+                dfs(i, 0, board, visited, deltaRow, deltaCol);
+            }
+            if (board[i][board[0].length - 1] == 1 && !visited[i][board[0].length - 1]) {
+                dfs(i, board[0].length - 1, board, visited, deltaRow, deltaCol);
+            }
+        }
+
+        // first row and last row
+        for (int i = 0; i < visited[0].length; i++) {
+            if (board[0][i] == 1 && !visited[0][i]) {
+                dfs(0, i, board, visited, deltaRow, deltaCol);
+            }
+            if (board[board.length - 1][i] == 1 && !visited[board.length - 1][i]) {
+                dfs(board.length - 1, i, board, visited, deltaRow, deltaCol);
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < visited.length; i++) {
+            for (int j = 0; j < visited[0].length; j++) {
+                if (!visited[i][j] && board[i][j] == 1) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private void dfs(int i, int j, int[][] board, boolean[][] visited, int[] deltaRow, int[] deltaCol) {
+        visited[i][j] = true;
+        for (int k = 0; k < 4; k++) {
+            int nrow = i + deltaRow[k];
+            int nCol = j + deltaCol[k];
+            if (nrow >= 0 && nrow < board.length && nCol >= 0 && nCol < board[0].length && board[nrow][nCol] == 1 && !visited[nrow][nCol]) {
+                dfs(nrow, nCol, board, visited, deltaRow, deltaCol);
+            }
+        }
+    }
+}
