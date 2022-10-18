7cffa85c1f3eddea3145559880643dc43acd7dcc
Meet Vora <47553376+vmeet24@users.noreply.github.com>
2022-10-18 01:27:18 -0400

    Time: 5 ms (97.64%), Space: 50.7 MB (93.41%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,28 +1,37 @@
 class Solution {
 
     public List<Integer> eventualSafeNodes(int[][] graph) {
+        boolean[] visited = new boolean[graph.length];
+        boolean[] isSafe = new boolean[graph.length];
         List<Integer> res = new ArrayList<>();
+        boolean[] pathVisited = new boolean[graph.length];
         for (int i = 0; i < graph.length; i++) {
-            if (checkSafeState(i, graph, new boolean[graph.length], new boolean[graph.length])) {
+            if (!visited[i]) {
+                checkSafeState(i, graph, visited, pathVisited, isSafe);
+            }
+        }
+        
+        for(int i = 0; i<isSafe.length; i++){
+            if(isSafe[i]){
                 res.add(i);
             }
         }
         return res;
     }
 
-    private boolean checkSafeState(int i, int[][] graph, boolean[] visited, boolean[] pathVisited) {
+    private boolean checkSafeState(int i, int[][] graph, boolean[] visited, boolean[] pathVisited, boolean[] isSafe) {
         pathVisited[i] = true;
         visited[i] = true;
-
         for (int neighbor : graph[i]) {
             if (!visited[neighbor]) {
-                if (!checkSafeState(neighbor, graph, visited, pathVisited)) {
+                if (!checkSafeState(neighbor, graph, visited, pathVisited, isSafe)) {
                     return false;
                 }
             } else if (pathVisited[neighbor]) {
                 return false;
             }
         }
+        isSafe[i] = true;
         pathVisited[i] = false;
         return true;
     }
